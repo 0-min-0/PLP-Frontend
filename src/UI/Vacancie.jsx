67121c96f409a5915e76b937169dcf532e4e40e5
0-min-0 <146240605+0-min-0@users.noreply.github.com>
2025-05-07 20:46:45 -0500

    Tarjetas de vacantes terminadas

--- src/UI/Vacancie.jsx
+++ src/UI/Vacancie.jsx
@@ -2,37 +2,38 @@ import React from 'react'
 import { IoBriefcaseOutline } from 'react-icons/io5'
 import { Button } from './button'
 
-export const Vacancie = () => {
+export const Vacancie = ({ title, company, location, type, experience }) => {
 
   const itemStyle = 'flex gap-2 mb-2'
   return (
-    <div className='bg-white rounded-lg border border-[#60efdb] px-8 py-6 text-[#405e7f]'>
+    <div className='bg-white rounded-xl border border-[#60efdb] px-8 py-6 text-[#405e7f] w-75 h-75 flex flex-col'>
       <div className='flex justify-between '>
-        <h2 className='mb-2 font-semibold text-lg'>Ocupación</h2>
-        <IoBriefcaseOutline className='w-5 h-5' />
+        <h2 className='mb-2 font-semibold text-lg line-clamp-2'>{title}</h2>
+        <IoBriefcaseOutline className='w-5 h-5 flex-shrink-0 ml-2' />
       </div>
-      <div>
+      <div className='flex-grow'>
         <div className={itemStyle}>
-          <h3 className='font-semibold'>Empresa - </h3>
-          <p>Tech Corp</p>
+          <h3 className='font-semibold'>Empresa • </h3>
+          <p className='truncate'>{company}</p>
         </div>
         <div className={itemStyle}>
-          <h3 className='font-semibold'>Ubicación - </h3>
-          <p>Armenia, Quindio</p>
+          <h3 className='font-semibold'>Ubicación • </h3>
+          <p className='truncate'>{location}</p>
         </div>
         <div className={itemStyle}>
-          <h3 className='font-semibold'>Disponibilidad - </h3>
-          <p>Medio Tiempo</p>
+          <h3 className='font-semibold'>Disponibilidad • </h3>
+          <p>{type}</p>
         </div>
         <div className={itemStyle}>
-          <h3 className='font-semibold'>Experiencia - </h3>
-          <p>Semi-senior/ 3 años</p>
+          <h3 className='font-semibold'>Experiencia • </h3>
+          <p>{experience}</p>
         </div>
       </div>
+
       <Button
-      btnName='Ver detalles' 
-      btnType='button'
-      btnStyle='bg-[#405e7f] text-white font-semibold w-full mt-2'
+        btnName='Ver detalles'
+        btnType='button'
+        btnStyle='bg-[#405e7f] text-white font-semibold w-full mt-2'
       />
     </div>
   )
