91de56443659b4b40f8a34623e39efdee30e83b2
0-min-0 <146240605+0-min-0@users.noreply.github.com>
2025-05-08 02:06:50 -0500

    Correcciones de despliegue 2.0

--- src/Components/AccountButtons/AccountButtons.jsx
+++ src/Components/AccountButtons/AccountButtons.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
-import { Button } from '../../UI/Button'
+import { Button } from '../../UI/button'
 
 export const AccountButtons = () => {
     return (
