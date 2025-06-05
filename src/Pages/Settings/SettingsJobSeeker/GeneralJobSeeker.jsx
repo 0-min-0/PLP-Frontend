e2d4bd5a9920757944d142dbf8a6ff308e91a389
0-min-0 <yookiim000@gmail.com>
2025-06-05 14:27:16 -0500

    cambios 2

--- src/Pages/Settings/SettingsJobSeeker/GeneralJobSeeker.jsx
+++ src/Pages/Settings/SettingsJobSeeker/GeneralJobSeeker.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import { Avatar } from '../../../Components/Avatar/Avatar'
+import { SearchBar } from '../../../UI/SearchBar'
 
 export const GeneralJobSeeker = () => {
   return (
-    <div>
-      General
-    </div>
+    <>
+      <Avatar />
+      <div className='max-w-5xl mx-auto mt-8'>
+        <SearchBar />
+        <h2 className='text-3xl mb-4 mt-6 font-[afacadBold] text-[#405e7f]'>
+          General
+        </h2>
+      </div>
+      <div>
+        
+      </div>
+    </>
   )
 }
 
